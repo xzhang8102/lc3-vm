d785efd8153fe898faf92ef1a40abed1a54f82b7
zxw <xzhang305@gmail.com>
2022-02-22 11:56:19 +0800

    trap routine

--- notes/main.c
+++ notes/main.c
@@ -62,6 +62,24 @@ enum
 };
 
 /**
+ * Trap Code
+ *
+ * Note:
+ * This is why programs start at address 0x3000 instead of 0x0.
+ * The lower addresses are left empty to leave space for the trap routine code.
+ * (In the official LC-3 simulator, trap routines are written in assembly.)
+ */
+enum
+{
+  TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
+  TRAP_OUT = 0x21,   /* output a character */
+  TRAP_PUTS = 0x22,  /* output a word string */
+  TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
+  TRAP_PUTSP = 0x24, /* output a byte string */
+  TRAP_HALT = 0x25   /* halt the program */
+};
+
+/**
  * LC-3 Assembly Examples:
  *
  * Hello World Assembly:
@@ -279,7 +297,68 @@ int main(int argc, char const *argv[])
     }
     break;
     case OP_TRAP:
-      // TODO: OP_TRAP
+      switch (instr & 0xFF)
+      {
+      case TRAP_GETC:
+      {
+        reg[R_R0] = (uint16_t)getchar();
+        update_flags(R_R0);
+      }
+      break;
+      case TRAP_OUT:
+      {
+        putc((char)reg[R_R0], stdout);
+        fflush(stdout);
+      }
+      break;
+      case TRAP_PUTS:
+      {
+        uint16_t *c = memory + reg[R_R0];
+        while (*c)
+        {
+          putc((char)*c, stdout);
+          ++c;
+        }
+        fflush(stdout);
+      }
+      break;
+      case TRAP_IN:
+      {
+        printf("Enter a charater: ");
+        char c = getchar();
+        putc(c, stdout);
+        fflush(stdout);
+        reg[R_R0] = (uint16_t)c;
+        update_flags(R_R0);
+      }
+      break;
+      case TRAP_PUTSP:
+      {
+        /**
+         * one char per byte (two bytes per word)
+         * here we need to swap back to big endian format
+         */
+        uint16_t *c = memory + reg[R_R0];
+        while (*c)
+        {
+          char char1 = (*c) & 0xFF;
+          putc(char1, stdout);
+          char char2 = (*c) >> 8;
+          if (char2)
+            putc(char2, stdout);
+          ++c;
+        }
+        fflush(stdout);
+      }
+      break;
+      case TRAP_HALT:
+      {
+        puts("HALT");
+        fflush(stdout);
+        running = 0;
+      }
+      break;
+      }
       break;
     case OP_RES:
     case OP_RTI:
