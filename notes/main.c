baf97d6ba7b370d0d4ffb2c6b86b31a7588fd9fa
zxw <xzhang305@gmail.com>
2022-02-22 11:26:36 +0800

    OP LEA

--- notes/main.c
+++ notes/main.c
@@ -249,8 +249,13 @@ int main(int argc, char const *argv[])
     }
     break;
     case OP_LEA:
-      // TODO: OP_LEA
-      break;
+    {
+      uint16_t r0 = (instr >> 9) & 0x7;
+      uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+      reg[r0] = reg[R_PC] + pc_offset;
+      update_flags(r0);
+    }
+    break;
     case OP_ST:
       // TODO: OP_ST
       break;
